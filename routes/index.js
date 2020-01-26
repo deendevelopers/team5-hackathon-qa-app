dc37683f8d70caa518f9b2e11856e33b4f358e4e
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2020-01-26 11:38:24 +0000

    Frontend journey now completed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mosque Q&A' });
+});
+
+router.get('/find-mosque', function(req, res, next) {
   res.render('find_mosque', { title: 'Mosque Q&A' });
+});
+
+router.get('/ask-question', function(req, res, next) {
+  res.render('question', { title: 'Mosque Q&A' });
+});
+
+router.post('/success', function(req, res, next) {
+  let data = {
+    category: req.body["category-id"],
+    question: req.body.question
+  };
+  res.render('success', { title: 'Mosque Q&A', ...data });
 });
 
 module.exports = router;
